aa8a2d6eaf4e2c72b6cd7ab538b08b4ffbfb3c1b
YH Yang <yhuiyang@gmail.com>
2014-12-04 20:32:07 +0800

    Add some model helper functions.

--- models/gcm_app.py
+++ models/gcm_app.py
@@ -10,21 +10,54 @@ from google.appengine.ext import ndb
 
 
 class GcmAppModel(ndb.Model):
-    '''
+    """
     All entities have the same parent ndb.Key(GcmAppModel, 'GcmApp')
     Entity's key = ndb.Key(GcmAppModel, apk_package_name)
-    '''
+    """
     display_name = ndb.StringProperty()
     sender_id = ndb.StringProperty(indexed=False)
     google_api_key = ndb.StringProperty(indexed=False)
     timestamp = ndb.DateTimeProperty(auto_now=True)
 
+    @classmethod
+    def get_instance(cls, apk_package_name):
+        return cls.get_by_id(id=apk_package_name, parent=ndb.Key(GcmAppModel, 'GcmApp'))
+
+    @classmethod
+    def check_exist(cls, apk_package_name):
+        return True if GcmAppModel.get_instance(apk_package_name) else False
+
 
 class GcmDeviceModel(ndb.Model):
-    uuid = ndb.StringProperty(indexed=False)
+    """
+    Entity's key = ndb.Key(GcmDeviceModel, registration_id)
+    Use GcmDeviceModel.get_by_id(id=<your_registration_id>) to get device entity for specific registration id
+    """
     package = ndb.StringProperty()
     version = ndb.IntegerProperty()
     timestamp = ndb.DateTimeProperty(auto_now=True)
+    enabled = ndb.BooleanProperty(default=True)
+
+    # below are custom properties
+    uuid = ndb.StringProperty(indexed=False)
+
+    @classmethod
+    def get_instance(cls, registration_id):
+        return cls.get_by_id(id=registration_id)
+
+    @classmethod
+    def disable_device(cls, registration_id):
+        entity = GcmDeviceModel.get_instance(registration_id)
+        if entity:
+            entity.enabled = False
+            entity.put()
+
+    @classmethod
+    def enable_device(cls, registration_id):
+        entity = GcmDeviceModel.get_instance(registration_id)
+        if entity:
+            entity.enabled = True
+            entity.put()
 
 
 class GcmDeviceDailyCountModel(ndb.Model):
